abb44a65085e59ccbf3f9590cf3661beee710aea
steven andre <andremartines2010@gmail.com>
2026-03-07 15:31:02 -0600

    modulacion

---
+++ lib/assets/pages/counter_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text("hola"))),
+    );
+  }
+}
