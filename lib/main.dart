8f8bd014dc506d690a15ff59237485eedaa726ab
steven andre <andremartines2010@gmail.com>
2026-03-07 11:06:19 -0600

    hola

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(home: Center(child: Text("hola")));
+  }
+}
