1711440841b720299341f9ebd13fa1cc1c2feecd
steven andre <andremartines2010@gmail.com>
2026-03-10 17:08:05 -0600

    app completa

--- lib/assets/pages/counter_screen.dart
+++ lib/assets/pages/counter_screen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
   @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text("hola"))),
+      home: Scaffold(
+        appBar: AppBar(title: Center(child: Text("Counter Screen"))),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Text(
+                "$count",
+                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              ),
+              Text(
+                count == 1 ? "click" : "clicks",
+                style: TextStyle(fontSize: 60, fontWeight: FontWeight.w100),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              count += 1;
+            });
+          },
+          child: const Icon(Icons.plus_one_sharp),
+        ),
+      ),
     );
   }
 }
